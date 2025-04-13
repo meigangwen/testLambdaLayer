ae935e9bdd63c803b5b47775a45afa9820b26be7
fancilivain <meigangwen@gmail.com>
2025-04-13 09:43:14 +0800

    Update test_lambda_layer_stack.py

--- test_lambda_layer/test_lambda_layer_stack.py
+++ test_lambda_layer/test_lambda_layer_stack.py
@@ -11,6 +11,13 @@ class TestLambdaLayerStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Reference public Lambda Layer using ARN
+        aws_sdk_pandas_layer = _lambda.LayerVersion.from_layer_version_arn(
+            self,
+            'AwsSdkPandasLayer',
+            'arn:aws:lambda:ap-southeast-1:336392948345:layer:AWSSDKPandas-Python312:5'
+        )
+
         initiator = _alambda.PythonFunction(
             self,
             "Main",
@@ -18,5 +25,6 @@ class TestLambdaLayerStack(Stack):
             runtime=_lambda.Runtime.PYTHON_3_9,
             index='main.py',
             handler="handle",
+            layers=[aws_sdk_pandas_layer],
             timeout=Duration.seconds(10)
         )
